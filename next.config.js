97c8402eac620a6c9e21b728e159d333a7aac7c2
aryasis87 <aryasis87@gmail.com>
2025-12-22 09:25:59 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  async rewrites() {
+    return [
+      {
+        source: '/api/backend/:path*',
+        destination: 'http://103.171.85.146:3000/api/v1/:path*',
+      },
+    ]
+  },
+  
   images: {
     domains: [],
     formats: ['image/avif', 'image/webp'],
